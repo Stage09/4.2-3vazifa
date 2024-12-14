eedafc5a4bb51ca39380044250882e4f81e3db99
Stage777999 <ikingib1979@gmail.com>
2024-12-14 12:52:19 +0500

    finished

---
+++ main.js
@@ -0,0 +1,93 @@
+// 1-vazifa    || https://carbon.now.sh/mYdp4oDr3HnMVNvPlEdX  ||
+
+// 1-masala
+
+// let yosh = prompt("Yoshingizni kiriting:"); 
+// yosh = Number(yosh);
+
+// if (yosh < 10) {
+//     alert("Salom kichkintoy!");
+// } else if (yosh < 18) {
+//     alert("Salom mehmon!");
+// } else if (yosh < 50) {
+//     alert("Salom amaki!");
+// } else if (yosh < 100) {
+//     alert("Salom bobo!");
+// } else if (yosh > 110) {
+//     alert("Buncha yosh yashalmaydi!");
+// } else {
+//     alert("Yoshni to'g'ri kiriting.");
+// }
+
+
+// 2-masala
+
+// let yosh = prompt("Yoshingizni kiriting:"); 
+// yosh = Number(yosh); 
+
+// if (yosh > 1 && yosh < 10) {
+//     alert("Salom kichkintoy!");
+// } else if (yosh >= 10 && yosh < 18) {
+//     alert("Salom mehmon!");
+// } else if (yosh >= 18 && yosh < 50) {
+//     alert("Salom amaki!");
+// } else if (yosh >= 50 && yosh < 100) {
+//     alert("Salom bobo!");
+// } else if (yosh >= 110) {
+//     alert("Buncha yosh yashalmaydi!");
+// } else {
+//     alert("Yoshni to'g'ri kiriting.");
+// }
+
+// 2-vazifa    || https://carbon.now.sh/Ngl2S8ztNjI1DFLR8kuc ||
+
+
+// let login = prompt("Loginingizni kiriting:");  
+
+// if (login === "admin") {  
+//     let parol = prompt("Parolingizni kiriting:");  
+//     if (parol === "12345") {  
+//         alert("Hush kelibsiz!");
+//     } else { 
+//         alert("Parol xato!");
+//     }
+// } else { 
+//     alert("Kirish man qilindi!");
+// }
+
+
+// 3-vazifa || https://carbon.now.sh/ngAOqtL1wQ64jrNqFfr8 ||
+
+// let age; 
+
+// age = prompt("Yoshingizni kiriting:");  
+// age = Number(age); 
+
+// if (age >= 18 && age <= 59) {
+//     alert("Siz hali ishlashingiz kerak.");
+// } else if (age > 59) {
+//     alert("Pensiyaga chiqish vaqti bo'ldi.");
+// } else if (age >= 1 && age <= 15) {
+//     alert("Ishlashga vohli borib o'qing.");
+// } else {
+//     alert("Yoshni to'g'ri kiriting.");
+// }
+
+
+// 4-vazifa
+
+let oy = prompt("Oyni kiriting (1-12 oralig'ida):");  
+oy = Number(oy);  
+
+if (oy >= 1 && oy <= 3) {
+    alert("Qish"); 
+} else if (oy >= 4 && oy <= 6) {
+    alert("Bahor");  
+} else if (oy >= 7 && oy <= 9) {
+    alert("Yoz"); 
+} else if (oy >= 10 && oy <= 12) {
+    alert("Kuz");  
+} else {
+    alert("Iltimos, 1 dan 12 gacha bo'lgan raqam kiriting.");
+}
+
